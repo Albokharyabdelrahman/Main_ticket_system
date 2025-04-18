61cec5f52dcb0f7e0781ae5d7259747b64e08817
Momen Ahmed <135729867+reaper121900@users.noreply.github.com>
2025-04-18 14:28:58 +0200

    MultiFactorAuthorization
    MultiFactor Authorization

--- models/User.js
+++ models/User.js
@@ -10,15 +10,17 @@ const userSchema = new mongoose.Schema(
       enum: ['Admin', 'Organizer', 'User'],
       required: true
     },
-    profilePicture: { type: String, 
-       otp: {
+    profilePicture: { type: String }, 
+    otp: {
       code: String,
       expiresAt: Date
     },
     otpVerified: {
       type: Boolean,
       default: false
-    }}
+    },
+  otpExpires: { type: Date }, // OTP expiration date
+
   },
   { timestamps: true } // ⏱️ Automatically adds createdAt and updatedAt
   
